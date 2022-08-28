b39e9a20bb59067b70a68e541359578b1933d02c
diamonluong2 <diamonluong2@gmail.com>
2022-08-28 13:22:57 +0700

    28-8-1

---
+++ 03_Luong/Project/Selectobject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+// using AutoCad
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows.Data;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Exception = Autodesk.AutoCAD.Runtime.Exception;
+using Autodesk.AutoCAD.Colors;
+
+// Thực hành tạo tool create new layer
+// Tùy theo cá nhân mỗi người có một cách viết logic khác nhau
+
+namespace Learning_API_Training
+{
+    public class SelectionAutoCad
+    {
+        [CommandMethod("SEL")]
+        public void selection()
+        {
+            // Đi tới bản vẽ hiện hành thực hiện câu lệnh
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+
+            PromptSelectionOptions pso = new PromptSelectionOptions();
+            pso.MessageForAdding = "Quet chon doi tuong";
+            pso.AllowDuplicates = false;
+            pso.RejectObjectsOnLockedLayers = true;
+
+            PromptSelectionResult psr = ed.GetSelection(pso);
+            SelectionSet ss = psr.Value;
+
+            foreach (ObjectId ob in ss.GetObjectIds())
+            {
+                if (ob.ObjectClass.Name == "AcDbCircle")
+                {
+                    using(Transaction tr = db.TransactionManager.StartTransaction())
+                    {
+                        Circle cc = tr.GetObject(ob,OpenMode.ForRead) as Circle;
+                        ed.WriteMessage("\nRadius la:" + cc.Radius);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 03_Luong/Project/note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_API_Training
+{
+    internal class note
+    {
+    // Lập trình có 3 loại
+    // _proproties: ctrl +1 để thấy thuộc tính dùng Snoop để xem
+    // _Method: ví dụ như copy (các kiểu hành động )
+    // 
+    }
+}
