429423aa30679ea8e1bd39e7465f01125bce3076
vietkz1997 <110753398+vietkz1997@users.noreply.github.com>
2022-09-10 17:10:51 +0700

    202020202

--- 05_Viet/ClassLibrary1/Class1.cs
+++ 05_Viet/ClassLibrary1/Class1.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
-namespace ClassLibrary1
+// using AutoCad
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows.Data;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+using Exception = Autodesk.AutoCAD.Runtime.Exception;
+using Autodesk.AutoCAD.Colors;
+
+namespace pratice
 {
-    public class Class1
+    public class newdim
     {
+        [CommandMethod("nd")]
+        public void cmdRun()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+            
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                DimStyleTable dst = tr.GetObject
+            }
+
+        }
     }
 }
